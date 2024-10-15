5a1cc547fadc10c7900ef77c234264763a0d90a4
salambayo3 <103267763+salambayo3@users.noreply.github.com>
2024-10-15 17:53:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+const menuBtn = document.getElementById("menu-btn")
+const navLinks = document.getElementById("nav-links")
+const menuBtnIcon = menuBtn.querySelector("i")
+
+menuBtn.addEventListener("click", (e) => {
+    navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "fas fa-close" : "fas fa-bars");
+});
+
+
+navLinks.addEventListener("click", (e) => {
+    navLinks.classList.remove("open")
+    menuBtnIcon.setAttribute("class", "fas fa-bars")
+})
+
+
+
+// scroll effect
+
+
+const scrollRevealOption = {
+    distnce: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+
+ScrollReveal().reveal(".header_img img", {
+    ...scrollRevealOption,
+    origin: "right",
+});
+
+
+ScrollReveal().reveal(".header_content h1", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+ScrollReveal().reveal(".header_content p", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+ScrollReveal().reveal(".header_content form", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
+ScrollReveal().reveal(".header_content .bar", {
+    ...scrollRevealOption,
+    delay: 2000,
+});
+
+
+ScrollReveal().reveal(".header_img_card", {
+    duration: 1000,
+    interval: 500,
+    delay: 2500,
+});
